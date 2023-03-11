b59d93053476850a6b3bfd5c3b3a97bab5e4b616
OleksiiMakoveiTeam <alex.m@cgsteam.io>
2023-03-12 00:21:20 +0200

    Fix typescript null error

--- src/test/helpers/withMarkup/withMarkup.ts
+++ src/test/helpers/withMarkup/withMarkup.ts
@@ -5,9 +5,10 @@ type Query = (f: MatcherFunction) => HTMLElement
 const withMarkup =
   (query: Query) =>
   (text: string): HTMLElement =>
-    query((_, node: Element) => {
+    query((content: string, node: Element | null) => {
+      if (!node) return false
       const hasText = (innerNode: HTMLElement) => innerNode.textContent === text
-      const childrenDontHaveText = Array.from(node.children).every(child => !hasText(child as HTMLElement))
+      const childrenDontHaveText = node && Array.from(node.children).every(child => !hasText(child as HTMLElement))
       return hasText(node as HTMLElement) && childrenDontHaveText
     })
 
